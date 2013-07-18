614f016210050c93cb45a4497a3c9fb720fe9cde
gcanteli <guillermocanteli@gmail.com>
2013-07-18 22:39:20 +0000

    modify web.js

--- web.js
+++ web.js
@@ -7,8 +7,8 @@ var fs = require('fs');
 var buffer = new Buffer(8);
 
 app.get('/', function(request, response) {
-   buffer.write(fs.readFileSync('./index.html', 'utf8');
-   buffer.toString('utf8');
+   buffer.write(fs.readFileSync('./index.html', 'utf-8');
+   buffer.toString('utf-8');
 });
 
 var port = process.env.PORT || 5000;
