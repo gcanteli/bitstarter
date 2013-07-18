44cfc22c437d98fa54996bd48d99bdb8a57275af
gcanteli <guillermocanteli@gmail.com>
2013-07-18 21:56:49 +0000

    modify web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var fs = require('fs');
+
+var buffer = new Buffer(8);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buffer.write(fs.readFileSync('index.html', encoding));
+   buffer.toString();
 });
 
 var port = process.env.PORT || 5000;
